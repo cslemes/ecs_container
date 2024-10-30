b22cd8cf40b6fc7c7239033f9a0eb1045b6ed7fe
Cristiano Lemes <cslemess@gmail.com>
2024-10-30 20:42:27 -0300

    fix databrick variables

--- terraform/databricks.tf
+++ terraform/databricks.tf
@@ -1,13 +1,11 @@
-data "databricks_aws_unity_catalog_assume_role_policy" "this" {
-  aws_account_id = data.aws_caller_identity.current.account_id
-  role_name      = local.uc_iam_role
-  external_id    = databricks_storage_credential.external.aws_iam_role[0].external_id
-}
 
-data "databricks_aws_unity_catalog_policy" "this" {
-  aws_account_id = data.aws_caller_identity.current.account_id
-  bucket_name    = aws_s3_bucket.external.id
-  role_name      = local.uc_iam_role
+data "databricks_spark_version" "latest_lts" {
+  depends_on = [databricks_mws_workspaces.this]
+  provider   = databricks.workspace
+}
+data "databricks_node_type" "smallest" {
+  depends_on = [databricks_mws_workspaces.this]
+  provider   = databricks.workspace
 }
 
 # Create a Databricks workspace using the cross-account IAM role

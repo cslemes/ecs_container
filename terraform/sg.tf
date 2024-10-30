a4cd8e9bd3d3eb7fbde04c641fe06434c05b1660
Cristiano Lemes <cslemess@gmail.com>
2024-10-30 20:24:47 -0300

    create git action

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -3,12 +3,6 @@ resource "aws_security_group" "aurora_sg" {
   description = "Security group for Aurora Serverless"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port   = 0
